f5eaa1ad291f4fb8795670bb887b9c11d726bfcb
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-08-31 10:18:26 -0600

    Add files via upload

--- pruebaarbolniveles.c
+++ pruebaarbolniveles.c
@@ -11,7 +11,10 @@ int main()
 	printf("el número que ingresó es %d", num1); */
 	
 	int a;
-	printf("¿De cuantos niveles quieres el arbol?\n" );
+	printf("Elige una opcion\n" );
+	printf("Arbol de 3 niveles:3\n" );
+	printf("Arbol de 5 niveles:5\n" );
+	printf("Arbol de 7 niveles:7\n" );
 	scanf("%d", &a);
 	
 	if( a == 3 )
@@ -27,14 +30,30 @@ int main()
 			if(a == 5 ) 
 			{
 				printf("arbol de 5 niveles\n" );
+				printf("\t\t\t      * ");
+				printf("\n\t\t\t     /|\\ ");
+				printf("\n\t\t\t    //|\\\\ ");
+				printf("\n\t\t\t   ///|\\\\\\ ");
+				printf("\n\t\t\t  ////|\\\\\\\\ ");
+				printf("\n\t\t\t /////|\\\\\\\\\\ ");
+				printf("\n\t\t\t    IIIII ");
 			} 
 			if(a == 7 )
 			{
 				printf("arbol de 7 niveles\n" );
+				printf("\t\t\t        * ");
+				printf("\n\t\t\t       /|\\ ");
+				printf("\n\t\t\t      //|\\\\ ");
+				printf("\n\t\t\t     ///|\\\\\\ ");
+				printf("\n\t\t\t    ////|\\\\\\\\ ");
+				printf("\n\t\t\t   /////|\\\\\\\\\\ ");
+				printf("\n\t\t\t  //////|\\\\\\\\\\\\ ");
+				printf("\n\t\t\t ///////|\\\\\\\\\\\\\\ ");
+				printf("\n\t\t\t     IIIIIII ");
 			}
-				else 
-				{
-					printf("\nIngresa otro número\n" );
-				}
+			else 
+			{
+				printf("\nIngresa otro número\n" );
+			}
 	return 0;
 	}
